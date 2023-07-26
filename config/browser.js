41077a583b0fa389b2ce5001c76ce36770a552bb
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-07-26 16:51:04 +0530

    cleanup

--- config/browser.js
+++ config/browser.js
@@ -5,7 +5,7 @@ async function startBrowser(){
     try{
         console.log("Opening the browser........");
         browser = await puppeteer.launch({
-            headless : "new",
+            headless : false,
             args: ["--disable-setuid-sandbox"],
             'ignoreHTTPErrors': true 
         })
